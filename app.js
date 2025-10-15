64a6cafaa0a8433db7abe750e0f0bb00bfb5e8fe
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-10-15 14:42:20 +0200

    Controlla su Postman che ritorni Server del mio Blog nell'html

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// import del framework di express
+// Import del framework di express
 const express = require("express");
 
 const app = express();
 
 const port = 3000;
+
+// Middleware per servire file statici
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.send("<h1>Server del mio Blog</h1>")
