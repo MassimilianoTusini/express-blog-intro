c0d529c7fefcfc381097b9b8641e6569074fc6c2
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-10-15 18:31:15 +0200

    Completa esercizio

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // Import del framework di express
 const express = require("express");
 
@@ -9,14 +10,46 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+    console.log("Richiesta arrivata su /");
     res.send("<h1>Server del mio Blog</h1>")
-})
+});
 
 
+app.get("/bacheca", (req, res) => {
+
+    const posts = [
+    {
+        title: "Ciambellone",
+        image: "http://localhost:3000/images/ciambellone.jpeg",
+        tags: ["", "", ""]
+    },
+    {
+        title: "Cracker Barbabietola",
+        image: "http://localhost:3000/images/cracker_barbabietola.jpeg",
+        tags: ["", "", ""]
+    },
+    {
+        title: "Pane fritto dolce",
+        image: "http://localhost:3000/images/pane_fritto_dolce.jpeg",
+        tags: ["", "", ""]
+    },
+    {
+        title: "Pasta Barbabietola",
+        image: "http://localhost:3000/images/pasta_barbabietola.jpeg",
+        tags: ["", "", ""]
+    },
+    {
+        title: "Torta Paesana",
+        image: "/images/torta_paesana.jpeg",
+        tags: ["", "", ""]
+    }
+];
+    // Invio della risposta con il json relativo
+    res.json( posts );
+});
 
 
-
-// ascolto della porta
+// Ascolto della porta
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+    console.log(`Server del Blog attivo su http://localhost:${port}`)
+});
